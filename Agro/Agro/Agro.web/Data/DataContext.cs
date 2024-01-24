fe4cab031b3f7de906d7eb3a0a5ef07705e8d06a
Carlos5808 <126487569+Carlos5808@users.noreply.github.com>
2024-01-24 11:17:27 -0600

    arquitectura de pryecto agro

---
+++ Agro/Agro/Agro.web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Agro.web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agro.web.Data
+{
+    public class DataContext DataContext : DbContext
+    {
+        DbSet<City> Cities { get; set; }
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {
+
+        }
+
+    protected override void OnModelCreating(ModelBuildermodeBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        ModelBuilder.Entity<City>().HasIndex(xx.Name).IsUnique();
+    }
+    }
+}
--- Agro/Agro/Agro.web/Data/Entities/City.cs
+++ Agro/Agro/Agro.web/Data/Entities/City.cs
@@ -1,6 +1,14 @@
-﻿namespace Agro.web.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agro.web.Data.Entities
 {
     public class City
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo  {0} es obligatorio")]
+        [Display(Name = "Ciudad")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+
+        public string? Name { get; set; }
     }
 }
